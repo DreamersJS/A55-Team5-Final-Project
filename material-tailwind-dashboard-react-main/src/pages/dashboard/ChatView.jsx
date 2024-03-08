3020f5a7e1ebdcb1aef6481d980522d1f141c29a
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-03-08 23:33:39 +0200

    chats

--- material-tailwind-dashboard-react-main/src/pages/dashboard/ChatView.jsx
+++ material-tailwind-dashboard-react-main/src/pages/dashboard/ChatView.jsx
@@ -14,21 +14,19 @@ import {
 import {
   EllipsisVerticalIcon,
 } from "@heroicons/react/24/outline";
-
+import { serverTimestamp } from "firebase/database";
 import { useContext, useEffect, useState } from "react";
-import { AppContext } from "../../appContext/AppContext";
 import { db } from "../../config.firebase/firebase-config";
 import { get, query, ref, push, update, orderByChild, equalTo } from "firebase/database";
 import { useNavigate, useParams } from "react-router-dom";
 import { getAllUsers } from "../../services/users.service";
 import { useRecoilState } from 'recoil';
 import { currentRoomId } from '../../atom/atom';
-// import { SingleChat } from "./SingleChat";
+import { AppContext } from "@/appContext/AppContext";
 
 export function ChatView() {
   const { user, userData } = useContext(AppContext);
   const [users, setUsers] = useState([]);
-  const [search, setSearch] = useState("");
   const [newMessage, setNewMessage] = useState("");
   const navigate = useNavigate();
   let { id } = useParams();
@@ -42,26 +40,34 @@ export function ChatView() {
   });
 
   const selectFriend = async (friend) => {
-      const participants = [user?.uid, friend.uid];
-      const room = await getRoom(participants);
+    const userId = user?.uid;
+    const friendId = friend.uid;
+    
+    if (!userId || !friendId) {
+        console.error("User ID or friend ID is missing.");
+        return;
+    }      
+    const participants = [userId, friendId];
+    const room = await getRoom(participants);
       console.log({ room });
       
       if (!room) {
           const newRoom = await createRoom(participants);
-           // console.log({newRoom});
+           console.log({newRoom});
           setRoom(newRoom);
       } else {
           setRoom(room);
       }
       setCurrentRoom(room.id);
       navigate(`/rooms/${room.id}`);
-      // console.log({currentRoom});
+      console.log({currentRoom});
   }
-  // useEffect(() => {
-  //     console.log("Current Room in useEffect:", currentRoom);
-  // }, [currentRoom]);
+  useEffect(() => {
+      console.log("Current Room in useEffect:", currentRoom);
+  }, [currentRoom]);
 
   const getRoom = async (participants) => {
+    console.log({participants});
       try {
           const roomsRef = ref(db, 'rooms');
           const snapshot = await get(roomsRef);
@@ -119,32 +125,86 @@ export function ChatView() {
       getAllUsers().then((users) => setUsers(users));
   }, []);
 
-  const handleSearchChange = async (e) => {
-      const value = e.target.value.toLowerCase();
-      setSearch(value);
+  //-------------------------------------------------------------------------------------
 
-      if (value.trim() === "") {
-          const updatedUsers = await getAllUsers();
-          setUsers(updatedUsers);
-      } else {
-          const snapshot = await get(query(ref(db, "users")));
+  const [messages, setMessages] = useState([]);
+
+  const checkRoomMessages = async (id = id) => {
+      const roomRef = ref(db, `rooms/${id}`);
+
+      try {
+          const snapshot = await get(roomRef);
+
           if (snapshot.exists()) {
-              const users = Object.keys(snapshot.val()).map((key) => ({
-                  id: key,
-                  ...snapshot.val()[key],
-              }));
+              console.log("Data exists:", snapshot.val());
 
-              const filteredUsers = users.filter((user) =>
-                  user.username.toLowerCase().includes(value) ||
-                  user.email.toLowerCase().includes(value)
-              );
-              setUsers(filteredUsers);
+              const messagesRef = child(roomRef, 'messages');
+              const messagesSnapshot = await get(messagesRef);
+              if (messagesSnapshot.exists()) {
+                  console.log("Messages exist:", messagesSnapshot.val());
+              } else {
+                  console.log("No messages yet in this room.");
+              }
+          } else {
+              console.log("Room does not exist.");
           }
+      } catch (error) {
+          console.error("Error accessing roomRef:", error);
       }
+  }
+
+  useEffect(() => {
+      try {
+          if (id) {
+
+              const roomRef = ref(db, `rooms/${id}/messages`);// useParams
+              const queryRef = query(roomRef);
+              const unsubscribe = onChildAdded(queryRef, (snapshot) => {
+                  const messageData = snapshot.val();
+                  console.log({ messageData});
+                  if (messageData) {
+                      setMessages((prevMessages) => [...prevMessages, messageData]);
+                  }
+              });
+              return () => {
+                  unsubscribe();
+              };
+
+          } else {
+              // If there's no current room, clear the messages
+              setMessages([]);
+          }
+      } catch (error) {
+          console.log("Error accessing roomRef:", error);
+      }
+  }, [currentRoom]);
+
+  const handleInputMessage = (e) => {
+      e.preventDefault();
+      const message = e.target.value;
+      setNewMessage(message);
   };
+
+  const sendMessage = async () => {
+      if (!newMessage.trim()) return;
+
+      const message = {
+          senderId: userData.uid,
+          senderName: userData.username,
+          content: newMessage,
+          timestamp: serverTimestamp(),
+          avatar: userData?.photoURL || null,
+      };
+
+      console.log("Message to be sent:", message);
+
+      await push(ref(db, `rooms/${currentRoom}/messages`), message);
+      setNewMessage("");
+  };
+
+
   return (
     <div className="mt-12">
-      
       
       <div className="mb-4 grid grid-cols-1 gap-6 xl:grid-cols-3">
         <Card className="overflow-scroll border border-blue-gray-100 shadow-sm">
@@ -199,35 +259,14 @@ export function ChatView() {
                 </tr>
               </thead>
               <tbody>
-                {users.length > 0 && users.map(
-                  ({ profilePhotoURL, username, uid, status }, key) => {
-                    const className = `py-3 px-5 ${
-                      key === users.length - 1
-                        ? ""
-                        : "border-b border-blue-gray-50"
-                    }`;
-
-                    return (
-                      <tr key={uid}>
-                        <td className={className}>
-                          <div className="flex items-center gap-4"   user={user} onClick={() => (selectFriend(user))} >
-                            <Avatar src={profilePhotoURL} alt='' size="sm" />
-                            <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className="font-bold"
-                            >
-                              {username}
-                            </Typography>
-                          </div>
-                        </td>
-                       
-                        
-                        
-                      </tr >
-                    );
-                  }
-                )}
+              <ul className="chat-user-list">
+                            {users.length > 0 &&
+                                users.map((user) => (
+                                    <li key = {user.id} className="px-5 py-[15px] group-data-[theme-color=violet]:hover:bg-slate-100 group-data-[theme-color=green]:hover:bg-green-50/50 group-data-[theme-color=red]:hover:bg-red-50/50 transition-all ease-in-out border-b border-white/20 dark:border-zinc-700 group-data-[theme-color=violet]:dark:hover:bg-zinc-600 group-data-[theme-color=green]:dark:hover:bg-zinc-600 group-data-[theme-color=red]:dark:hover:bg-zinc-600 dark:hover:border-zinc-700">
+                                      <h5 user={user} key={user.id} onClick={() => (selectFriend(user))}>{user.username}</h5>
+                                    </li>
+                                ))}
+                        </ul>
               </tbody>
             </table>
           </CardBody>
@@ -247,6 +286,27 @@ export function ChatView() {
           </CardHeader>
           <CardBody className="pt-0">
             <p>Select a chat</p>
+            { id ?  checkRoomMessages(id) : <p>Select a friend to start a chat.</p>}
+            {messages.length > 0 &&
+                         messages.map((message) => (
+                            <div key={message.messageId} className="flex items-center gap-4">
+                                                        <img src={message?.avatar} alt="" className="rounded-full h-9 w-9" />
+                                     <p>
+                                                                    {message.content}
+                                     </p>
+                            </div>
+                               ) )
+                                }
+
+                                    {/* handleSendMessage    sendMessage(messages) */}
+<div className="flex-grow">
+                                    <input type="text" value={newMessage} onChange={handleInputMessage} onClick={() => { sendMessage(messages) }} onKeyDown={(e) => {
+                                        if (e.key === 'Enter') {
+                                            e.preventDefault();
+                                            sendMessage();
+                                        }
+                                    }} className="w-full border-transparent rounded bg-gray-50 placeholder:text-14 text-14 dark:bg-zinc-700 dark:placeholder:text-gray-300 dark:text-gray-300" placeholder="Enter Message..." />
+                                </div>
           </CardBody>
         </Card>
       </div>
